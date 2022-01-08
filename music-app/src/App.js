ee52953e018e0f678c3ae5547b1aa09689eb37c6
“talhalalala” <talhasadak@outlook.comgit config --global user.name Talhalalala>
2022-01-08 17:03:32 +0000

    solved compiler issues

---
+++ music-app/src/App.js
@@ -0,0 +1,12 @@
+import Artist_details from "./components/Artist_details";
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <Artist_details /> 
+    </>
+  )
+};
+
+export default App;
---
+++ music-app/src/components/Artist_details.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Artist_details = () => {
+    return (
+        <div>
+            <h1>Smino</h1>
+            <img src='https://fq8ku9wqwk7gai1z3frl16nd-wpengine.netdna-ssl.com/wp-content/uploads/2020/04/smino-rev-cover-1200x630-1-996x515.jpg' alt='Smino in a bath' />
+            <h4>Christopher Smith Jr.</h4>
+            <h5>Born: October 2, 1991 (age 30)</h5>
+
+            
+                <h3> Music Genres</h3>
+                <ul>
+                    <li>Hip Hop</li>
+                    <li>Neo Soul</li>
+                    <li>R&B</li>
+                    <li>Electro-Funk</li>
+                </ul>
+            
+
+            
+                <h3>About</h3>
+                <p>He is a musician that makes music so he can pay the bills.</p>
+            
+        </div>
+    )
+}
+
+export default Artist_details;
